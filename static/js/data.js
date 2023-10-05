e876102f472463acd0fa11b2de4d66108526a404
iflidrie <395379722@qq.com>
2023-10-06 04:32:54 +0800

    first commit

---
+++ static/js/data.js
@@ -0,0 +1,16 @@
+const routes = [
+  {name: 'BL解析', url: 'https://svip.bljiex.cc/?v='}, 
+  {name: '虾米解析', url: 'https://jx.xmflv.com/?url='},
+  {name: 'JSON解析', url: 'https://jx.jsonplayer.com/player/?url='},
+  {name: '影猫の仓库 ', url: 'https://search.ymck.me/index.php?q='},
+  {name: '盘古解析 ', url: 'https://www.pangujiexi.cc/jiexi.php?url='},
+  {name: '线路1 ', url: 'https://yparse.jn1.cc/index.php?url='},
+  {name: '线路2 ', url: 'https://jx.aidouer.net/?url='},
+  {name: '线路3 ', url: 'https://www.1717yun.com/jx/ty.php?url='},
+  {name: '稳定通用', url: 'https://www.nxflv.com/?url='},
+  {name: '高速稳定', url: 'https://jx.jsonplayer.com/player/?url='}
+]
+
+const friends = [
+  {name: '德瑞', url: 'https://www.drie.cn/'},
+]
